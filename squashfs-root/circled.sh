dfa5c059879c9262f60025fc236e346ed281094b
Damian Pfammatter <pfammatter.damian@gmail.com>
2023-10-05 15:44:57 +0200

    Add option to run without GDB

--- squashfs-root/circled.sh
+++ squashfs-root/circled.sh
@@ -10,10 +10,26 @@ cp /libnvram.so /firmadyne/libnvram.so
 mkdir -p /tmp/media/nand/
 mount -t ramfs ramfs /tmp/media/nand/
 
-# Run preloaded target binary with gdbserver
-/usr/bin/gdbserver \
-    --wrapper env 'LD_PRELOAD="/libcircled.so /firmadyne/libnvram.so /lib/libdl.so.0"' -- \
-    127.0.0.1:3000 /bin/circled start
+# Parse command line arguments
+use_gdb=false
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --gdb) use_gdb=true;
+    esac
+    shift
+done
+
+# Run preloaded target binary
+if [ "$use_gdb" = true ]; then
+    # With gdbserver
+    /usr/bin/gdbserver \
+        --wrapper env 'LD_PRELOAD="/libcircled.so /firmadyne/libnvram.so /lib/libdl.so.0"' -- \
+        127.0.0.1:3000 /bin/circled start
+else
+    # Without gdbserver
+    LD_PRELOAD="/libcircled.so /firmadyne/libnvram.so /lib/libdl.so.0" /bin/circled start
+    sleep 30
+fi
 
 # Kill all leftover processes from the taraget
 kill -9 $(ps w | grep [/]bin/circled | awk '{print $1}')
