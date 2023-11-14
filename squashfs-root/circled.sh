0e65c9d3a668a10d039c1c09896b5fea6ba40b44
Damian Pfammatter <pfammatter.damian@gmail.com>
2023-11-14 13:13:26 +0100

    Terminate after exploit succeeded

--- squashfs-root/circled.sh
+++ squashfs-root/circled.sh
@@ -2,11 +2,13 @@
 export SHELL=/bin/sh
 
 # NVRAM emulator
+echo "[>] Setup NVRAM emulator"
 mkdir -p /firmadyne/libnvram/
 mkdir -p /firmadyne/libnvram.override/
 cp /libnvram.so /firmadyne/libnvram.so
 
 # Mount NAND media
+echo "[>] Mount NAND media"
 mkdir -p /tmp/media/nand/
 mount -t ramfs ramfs /tmp/media/nand/
 
@@ -22,21 +24,30 @@ done
 # Run preloaded target binary
 if [ "$use_gdb" = true ]; then
     # With gdbserver
+    echo "[>] Launch circled binary with GDB attached"
     /usr/bin/gdbserver \
         --wrapper env 'LD_PRELOAD="/libcircled.so /firmadyne/libnvram.so /lib/libdl.so.0"' -- \
-        127.0.0.1:3000 /bin/circled start
+        127.0.0.1:3000 /bin/circled.patched start
 else
     # Without gdbserver
-    LD_PRELOAD="/libcircled.so /firmadyne/libnvram.so /lib/libdl.so.0" /bin/circled start
-    sleep 30
+    echo "[>] Launch circled binary without GDB attached"
+    LD_PRELOAD="/libcircled.so /firmadyne/libnvram.so /lib/libdl.so.0" /bin/circled.patched start
+    while [ ! -e "/tmp/st0" ]; do
+        sleep 1
+    done
+    sleep 5
 fi
 
-# Kill all leftover processes from the taraget
+# Kill all leftover processes from the target binary
+echo "[>] Kill circled binary"
 kill -9 $(ps w | grep [/]bin/circled | awk '{print $1}')
 
 # Clean up
+echo "[>] Clean up"
 umount /mnt/
 umount /tmp/media/nand/
-rm -rf /tmp/media/nand/
 umount /firmadyne/libnvram
 rm -rf /firmadyne/
+cat /tmp/circled.log
+rm -rf /tmp/*
+echo "[<] Done"
