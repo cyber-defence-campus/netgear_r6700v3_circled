83b7e6408bf83c3e78525183be1b88428433fc3d
Damian Pfammatter <pfammatter.damian@gmail.com>
2023-11-16 15:13:02 +0100

    Cleanup

--- firmware/circled.sh
+++ firmware/circled.sh
@@ -2,13 +2,13 @@
 export SHELL=/bin/sh
 
 # NVRAM emulator
-echo "[>] Setup NVRAM emulator"
+echo "[+] Setup NVRAM emulator"
 mkdir -p /firmadyne/libnvram/
 mkdir -p /firmadyne/libnvram.override/
 cp /libnvram.so /firmadyne/libnvram.so
 
 # Mount NAND media
-echo "[>] Mount NAND media"
+echo "[+] Mount NAND media"
 mkdir -p /tmp/media/nand/
 mount -t ramfs ramfs /tmp/media/nand/
 
@@ -24,13 +24,13 @@ done
 # Run preloaded target binary
 if [ "$use_gdb" = true ]; then
     # With gdbserver
-    echo "[>] Launch circled binary with GDB attached"
+    echo "[+] Launch circled binary with GDB attached"
     /gdbserver \
         --wrapper env 'LD_PRELOAD="/libcircled.so /firmadyne/libnvram.so /lib/libdl.so.0"' -- \
         127.0.0.1:3000 /bin/circled.patched start
 else
     # Without gdbserver
-    echo "[>] Launch circled binary without GDB attached"
+    echo "[+] Launch circled binary without GDB attached"
     LD_PRELOAD="/libcircled.so /firmadyne/libnvram.so /lib/libdl.so.0" /bin/circled.patched start
     while [ ! -e "/tmp/st0" ]; do
         sleep 1
@@ -39,15 +39,15 @@ else
 fi
 
 # Kill all leftover processes from the target binary
-echo "[>] Kill circled binary"
+echo "[+] Kill circled binary"
 kill -9 $(ps w | grep [/]bin/circled | awk '{print $1}')
 
 # Clean up
-echo "[>] Clean up"
+echo "[+] Clean up"
 umount /mnt/
 umount /tmp/media/nand/
 umount /firmadyne/libnvram
 rm -rf /firmadyne/
 cat /tmp/circled.log
 rm -rf /tmp/*
-echo "[<] Done"
+echo "[+] Done"
